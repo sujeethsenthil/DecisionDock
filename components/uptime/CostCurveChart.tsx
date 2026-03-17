cca2aacc38bcef56e8025bf4104afa63649d684e
Sujeeth Senthilvel <Senthilvel.Su@northeastern.edu>
2026-03-17 09:53:41 -0400

    Transition fix

--- components/uptime/CostCurveChart.tsx
+++ components/uptime/CostCurveChart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import {
   AreaChart, Area, XAxis, YAxis, CartesianGrid, ReferenceLine,
   ReferenceArea, Tooltip, ResponsiveContainer,
